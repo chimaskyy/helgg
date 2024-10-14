add2747ba774383bb21e5f42124e15ab61ef23a1
chimaskyy <chiamakahj@gmail.com>
2024-10-14 21:26:52 +0100

    add form to application page

--- client/src/pages/Download.jsx
+++ client/src/pages/Download.jsx
@@ -17,42 +17,42 @@ function Download() {
                 Don’t forget to leave us a rating or feedback on the <br /> app
                 store so we can build better features for you.
               </p>
-              <div className="pt-12 flex gap-16  lg:flex-row">
+              <div className="w-full pt-12 flex gap-4 ml-4 lg:ml-0 ">
                 <a
                   href="https://play.google.com/store/apps/details?id=com.helggscooters.android&pli=1"
                   target="_blank"
-                  aria-label="Download from Google Play Store"
+                  aria-label="Google Play Store"
                   rel="noreferrer noopener"
                 >
                   <button
-                    aria-label="Download from Google Play Store"
-                    className="text-black group relative font-roboto  sm:px-6  sm:py-4 text-lg sm:text-sm rounded-full shadow-xl transition-transform transform bg-customGreen border-2 border-white hover:scale-105 hover:border-green-600 hover:shadow-green-500/50 hover:shadow-3xl focus:outline-none lg:text-lg lg:p-6"
+                    aria-label="Google Play Store"
+                    className="h-12 w-full group text-black  relative px-4 sm:px-6 py-2 sm:py-4 text-xs sm:text-sm rounded-full shadow-xl transition-transform transform bg-customGreen border-2 border-white hover:scale-105 hover:border-green-600  hover:shadow-3xl focus:outline-none md:w-full h-16 lg:w-52 lg:h-16"
                     id="downloadButton"
                   >
                     <FontAwesomeIcon
                       icon={faGooglePlay}
                       className="mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
                     />
-                    <span className="truncate text-lg">Google Play Store</span>
+                    <span className="truncate">Google Play Store</span>
                   </button>
                 </a>
 
                 <a
                   href="https://apps.apple.com/gb/app/helgg-rideelectric/id1626797004"
                   target="_blank"
-                  aria-label="Download from Apple App Store"
+                  aria-label="Apple Play Store"
                   rel="noreferrer noopener"
                 >
                   <button
-                    aria-label="Download from Apple App Store"
-                    className="text-white group relative font-roboto  sm:px-6  sm:py-4 text-lg sm:text-sm rounded-full shadow-xl transition-transform transform  border-2 border-white hover:scale-105 hover:border-green-600 hover:shadow-green-500/50 hover:shadow-3xl focus:outline-none lg:text-lg lg:p-6"
+                    aria-label="Apple Play Store"
+                    className="h-12 text-white w-full group relative px-4 sm:px-6 py-2 sm:py-4 text-xs sm:text-sm rounded-full shadow-xl transition-transform transform bg-transparent border-2 border-customGreen hover:scale-105 hover:border-green-600  hover:shadow-3xl focus:outline-none md:w-full h-16 lg:w-52 lg:h-16"
                     id="downloadButton"
                   >
                     <FontAwesomeIcon
                       icon={faApple}
                       className="mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
                     />
-                    <span className="truncate text-lg">Apple App Store</span>
+                    <span className="truncate">Apple Play Store</span>
                   </button>
                 </a>
               </div>
